b4fabde127ef383fd1cb90762e096a754843d647
siavash <siyavashfd@gmail.com>
2020-11-19 22:37:09 +0330

    Initial empty commit

---
+++ src/main/java/ir/fallahdoust/ignitespring/IgniteSpringApplication.java
@@ -0,0 +1,13 @@
+package ir.fallahdoust.ignitespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IgniteSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ir/fallahdoust/ignitespring/IgniteSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.fallahdoust.ignitespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgniteSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
